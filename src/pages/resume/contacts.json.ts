e24c5a12df48b03a9fdf84b9527ec291cee89532
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2023-11-15 06:27:13 +0700

    feat: Back to Astro

---
+++ src/pages/resume/contacts.json.ts
@@ -0,0 +1,18 @@
+export async function GET({}) {
+    return new Response(
+      JSON.stringify([
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/alif-yasa',
+        },
+        {
+          name: 'GitHub',
+          url: 'https://github.com/alifyasa',
+        },
+        {
+          name: 'email',
+          url: 'mailto://contact@alifyasa.dev',
+        },
+      ].sort((a, b) => a.name.localeCompare(b.name)))
+    )
+  }
